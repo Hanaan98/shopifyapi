1518b8c2963b1e51c617367c5bb96e07b6495352
Hanaan98 <bcsf20m502@pucit.edu.pk>
2024-03-07 01:35:53 +0500

    Some changes to index.js

--- index.js
+++ index.js
@@ -17,13 +17,46 @@ app.get("/feed", async (req, res) => {
     let jsonProducts = [];
 
     do {
-      const products = await shopify.product.list(params);
-      jsonProducts = jsonProducts.concat(products);
-      params = products.nextPageParameters;
-    } while (params !== undefined);
+      try {
+        const products = await shopify.product.list(params);
+        jsonProducts = jsonProducts.concat(products);
+        params = products.nextPageParameters;
+      } catch (error) {
+        console.error("Error fetching products:", error);
 
+        continue;
+      }
+    } while (params !== undefined);
+    const transformedData = jsonProducts.map((product) => ({
+      id: product.id,
+      mpn: product.variants[0].sku,
+      title: product.title,
+      item_group_id: product.variants[0].sku,
+      link: `https://daytonahelmets.com/products/${product.handle}`,
+      MSRP: product.variants[0].price,
+      wholesale_price: "",
+      image_link: product.image ? product.image.src : "",
+      other_image_url1: "",
+      other_image_url2: "",
+      other_image_url3: "",
+      other_image_url4: "",
+      other_image_url5: "",
+      other_image_url6: "",
+      other_image_url7: "",
+      other_image_url8: "",
+      product_type: product.product_type,
+      brand: product.vendor,
+      availability:
+        product.variants[0].inventory_quantity > 0
+          ? "in stock"
+          : "out of stock",
+      qty_on_hand: product.variants[0].inventory_quantity,
+      shipping_weight: product.variants[0].weight,
+      description: product.body_html,
+      upc_code: product.variants[0].barcode,
+    }));
     // Convert to CSV
-    const csvData = json2csv(jsonProducts);
+    const csvData = json2csv(transformedData);
 
     // Write CSV file
     fs.writeFile("products.csv", csvData, (err) => {
